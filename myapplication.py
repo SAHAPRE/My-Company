3ae820398a284075d50d53aa45c615b61164acb7
SAHAPRE <114910297+SAHAPRE@users.noreply.github.com>
2024-12-23 01:56:48 +0530

    Update myapplication.py

--- myapplication.py
+++ myapplication.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1 style='color:blue'>Hello There22, I am here to help you in Every ways!</h1>"
+    return "<h1 style='color:Green'>Hello World, I am here to help you in Every ways!</h1>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
